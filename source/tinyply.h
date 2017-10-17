4846dc6f6cfd215d99fe3c864b1f658bd179999d
jmespadero <josemiguel.espadero@urjc.es>
2017-10-17 02:14:02 +0200

    Small typo

--- source/tinyply.h
+++ source/tinyply.h
@@ -111,6 +111,6 @@ namespace tinyply
         void add_properties_to_element(const std::string & elementKey, const std::initializer_list<std::string> propertyKeys, const Type type, const size_t count, uint8_t * data, const Type listType, const size_t listCount);
 	};
 
-} // namesapce tinyply
+} // namespace tinyply
 
 #endif // tinyply_h
